b81da6fbf1693d4d8bddeb7d7f6b4d4f13969ac7
Shouryan Nikam <dev.shouryan@gmail.com>
2024-10-12 21:11:07 -0700

    undo toast on home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,11 @@ import '../App.css'
 
 import { Link, createFileRoute } from '@tanstack/react-router'
 
-import { Button } from '@/components/ui/button'
-import { useToast } from '@/components/hooks/use-toast'
-
 export const Route = createFileRoute('/')({
   component: App,
 })
 
 function App() {
-  const { toast } = useToast()
-  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center">
       <h1 className="text-3xl mb-8">week<span className='font-bold'>ability</span></h1>
@@ -20,16 +15,6 @@ function App() {
       <Link href='/transactions'>Transactions</Link>
       <Link href='/accounts'>Accounts</Link>
       <Link href='/investments'>Investments</Link>
-      <Button
-      onClick={() => {
-        toast({
-          title: "Scheduled: Catch up",
-          description: "Friday, February 10, 2023 at 5:57 PM",
-        })
-      }}
-    >
-      Show Toast
-    </Button>
     </div>
   )
 }
