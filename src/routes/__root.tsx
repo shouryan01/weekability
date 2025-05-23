b8cbbc2916ad8fd6d456cb7f3d8387563ddc13b4
Shouryan Nikam <dev.shouryan@gmail.com>
2025-05-22 17:15:47 -0700

    fix cookie error

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,24 +1,25 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { SidebarProvider } from '@/components/ui/sidebar'
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
 
-import { AppSidebar } from '@/components/app-sidebar'
-import { Toaster } from 'sonner'
+import { AppSidebar } from "@/components/app-sidebar";
+import { Toaster } from "sonner";
 
 export const Route = createRootRoute({
-  component: RootComponent
-})
+	component: RootComponent,
+});
 
 function RootComponent() {
-    const cookies = document.cookie.split('; ');
-    const cookie = cookies.find((c) => c.startsWith('sidebar:state='));
-    // @ts-ignore
-    const cookieIsOpen = decodeURIComponent(cookie.split('=')[1]) === "false";
+	const cookies = document.cookie.split("; ");
+	const cookie = cookies.find((c) => c.startsWith("sidebar:state="));
+	const cookieIsOpen = cookie
+		? decodeURIComponent(cookie.split("=")[1]) === "false"
+		: false;
 
-    return (
-      <SidebarProvider defaultOpen={cookieIsOpen}>
-        <AppSidebar />
-        <Outlet />
-        <Toaster richColors closeButton />
-      </SidebarProvider>
-    )
+	return (
+		<SidebarProvider defaultOpen={cookieIsOpen}>
+			<AppSidebar />
+			<Outlet />
+			<Toaster richColors closeButton />
+		</SidebarProvider>
+	);
 }
