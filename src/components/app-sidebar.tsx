5fea39bfbaa215a2042b83d0020368453ab3e5af
Shouryan Nikam <dev.shouryan@gmail.com>
2025-05-24 21:35:02 -0700

    fix sidebar buttons

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -108,31 +108,33 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 
 			<SidebarFooter className="mx-2.5">
 				{isOpen && (
-					<SidebarMenu>
-						<SidebarMenuItem>
-							<SidebarMenuButton asChild>
-								<Button
-									variant="outline"
-									onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-								>
-									<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 hover:rotate-45 transition-all duration-150 dark:-rotate-90 dark:scale-0" />
-									<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 hover:rotate-45 transition-all duration-150 dark:rotate-0 dark:scale-100" />
-								</Button>
-							</SidebarMenuButton>
-						</SidebarMenuItem>
-					</SidebarMenu>
-				)}
+					<div className="flex flex-row gap-1">
+						<SidebarMenu>
+							<SidebarMenuItem>
+								<SidebarMenuButton asChild>
+									<Button
+										variant="outline"
+										onClick={() =>
+											setTheme(theme === "light" ? "dark" : "light")
+										}
+									>
+										<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 hover:rotate-45 transition-all duration-150 dark:-rotate-90 dark:scale-0" />
+										<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 hover:rotate-45 transition-all duration-150 dark:rotate-0 dark:scale-100" />
+									</Button>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+						</SidebarMenu>
 
-				{isOpen && (
-					<SidebarMenu>
-						<SidebarMenuItem>
-							<SidebarMenuButton asChild>
-								<Button variant="outline" onClick={toggleSidebar}>
-									<UnfoldHorizontal className="h-[1.2rem] w-[1.2rem] transition-all duration-150" />
-								</Button>
-							</SidebarMenuButton>
-						</SidebarMenuItem>
-					</SidebarMenu>
+						<SidebarMenu>
+							<SidebarMenuItem>
+								<SidebarMenuButton asChild>
+									<Button variant="outline" onClick={toggleSidebar}>
+										<UnfoldHorizontal className="h-[1.2rem] w-[1.2rem] transition-all duration-150" />
+									</Button>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+						</SidebarMenu>
+					</div>
 				)}
 
 				{!isOpen && (
